c1502346093135fa489b0793f6ee2a6140cd5500
Grover Cristobal <elvis.cristobal.garrido@gmail.com>
2025-02-18 19:47:36 -0500

    database - inserción data prueba funciones y triggers

---
+++ database/seeds/002_test_data.sql
@@ -0,0 +1,13 @@
+-- Probar trigger de SKU
+INSERT INTO products (seller_id, name, description, price, stock, image_url) 
+VALUES (1, 'Licuadora Profesional Jugo Fruta 2 L 1600w Daewo Color Negro', 'Disfruta de deliciosos jugos y batidos con la Licuadora Profesional Daewoo DIBL-210E. Con su potente motor de 1600W, esta licuadora te permite preparar tus bebidas favoritas de manera rápida y eficiente.', 146, 15, 'http://http2.mlstatic.com/D_617016-MLA74180198285_012024-O.jpg');
+
+-- Ver el SKU generado
+SELECT sku, name FROM products ORDER BY id DESC LIMIT 1;
+
+-- Probar trigger de order_number
+INSERT INTO orders (group_id, seller_id, recipient_id, delivery_channel, current_status) 
+VALUES (1, 1, 1, 'Delivery', 'registrado');
+
+-- Ver el order_number generado
+SELECT order_number FROM orders ORDER BY id DESC LIMIT 1;
